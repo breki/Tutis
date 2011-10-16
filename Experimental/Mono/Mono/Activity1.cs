60749fc6ba9a3916ff794ac517b2f68282f6ab3f
breki74 <226863+breki@users.noreply.github.com>
2011-10-16 21:19:48 +0200

    Playing with Monodroid.

---
+++ Experimental/Mono/Mono/Activity1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Mono
+{
+    [Activity(Label = "Mono", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            TextView tv = new TextView(this);
+            tv.Text = "test";
+            SetContentView(tv);
+        }
+    }
+}
+
