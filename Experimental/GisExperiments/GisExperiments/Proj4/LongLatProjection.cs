18d1ab8713834120a35374614ac3ebb199b9ddd9
Igor <Igor@Jazz.triera.net>
2012-01-17 08:16:16 +0100

    WGS84 -> OSGB transformation now works (although not totally precise).

--- Experimental/GisExperiments/GisExperiments/Proj4/LongLatProjection.cs
+++ Experimental/GisExperiments/GisExperiments/Proj4/LongLatProjection.cs
@@ -2,25 +2,25 @@ using Brejc.Geometry;
 
 namespace GisExperiments.Proj4
 {
-    public class LongLatProjection : IProjection
+    public class LongLatProjection : SrsBase
     {
-        public string ProjectionCode
+        public override string ProjectionCode
         {
             get { return "longlat"; }
         }
 
-        public string ProjectionName
+        public override string ProjectionName
         {
             get { return "LongLat"; }
         }
 
-        public void inverse (double?[] coords)
+        public override void inverse (double?[] coords)
         {
             coords[0] = GeometryUtils.Deg2Rad (coords[0].Value);
             coords[1] = GeometryUtils.Deg2Rad (coords[1].Value);
         }
 
-        public void forward (double?[] coords)
+        public override void forward (double?[] coords)
         {
             coords[0] = GeometryUtils.Rad2Deg(coords[0].Value);
             coords[1] = GeometryUtils.Rad2Deg (coords[0].Value);
--- Experimental/GisExperiments/GisExperiments/Proj4/TransverseMercatorProjection.cs
+++ Experimental/GisExperiments/GisExperiments/Proj4/TransverseMercatorProjection.cs
@@ -2,67 +2,76 @@ using System;
 
 namespace GisExperiments.Proj4
 {
-    public class TransverseMercatorProjection : IProjection
+    public class TransverseMercatorProjection : SrsBase
     {
-        public string ProjectionCode
+        public override string ProjectionCode
         {
             get { return "tmerc"; }
         }
 
-        public string ProjectionName
+        public override string ProjectionName
         {
             get { return "Transverse Mercator"; }
         }
 
-        public void inverse (double?[] coords)
+        public override void inverse (double?[] coords)
         {
             throw new System.NotImplementedException();
         }
 
-        public void forward (double?[] coords)
+        public override void forward (double?[] coords)
         {
             double lon = coords[0].Value;
             double lat = coords[1].Value;
 
-            var delta_lon = ProjectionUtils.adjust_lon (lon - this.long0); // Delta longitude
-            var con;    // cone constant
-            var x, y;
-            var sin_phi = Math.Sin (lat);
-            var cos_phi = Math.Cos (lat);
+            double delta_lon = ProjectionUtils.adjust_lon (lon - long0); // Delta longitude
+            double con;    // cone constant
+            double x, y;
+            double sin_phi = Math.Sin (lat);
+            double cos_phi = Math.Cos (lat);
 
-            if (this.sphere)
+            if (Ellipsoid.IsSphere)
             {  /* spherical form */
-                var b = cos_phi * Math.Sin (delta_lon);
+                double b = cos_phi * Math.Sin (delta_lon);
                 if ((Math.Abs (Math.Abs (b) - 1.0)) < .0000000001)
-                {
-                    Proj4js.reportError ("tmerc:forward: Point projects into infinity");
-                    return (93);
-                }
-                else
-                {
-                    x = .5 * this.a * this.k0 * Math.log ((1.0 + b) / (1.0 - b));
-                    con = Math.Acos (cos_phi * Math.Cos (delta_lon) / Math.Sqrt (1.0 - b * b));
-                    if (lat < 0) con = -con;
-                    y = this.a * this.k0 * (con - this.lat0);
-                }
+                    throw new InvalidOperationException("tmerc:forward: Point projects into infinity");
+
+                x = .5 * Ellipsoid.a * k0.Value * Math.Log ((1.0 + b) / (1.0 - b));
+                con = Math.Acos (cos_phi * Math.Cos (delta_lon) / Math.Sqrt (1.0 - b * b));
+                if (lat < 0) con = -con;
+                y = Ellipsoid.a * k0.Value * (con - lat0);
             }
             else
             {
                 var al = cos_phi * delta_lon;
                 var als = Math.Pow (al, 2);
-                var c = this.ep2 * Math.Pow (cos_phi, 2);
+                var c = Datum.ep2 * Math.Pow (cos_phi, 2);
                 var tq = Math.Tan (lat);
                 var t = Math.Pow (tq, 2);
-                con = 1.0 - this.es * Math.Pow (sin_phi, 2);
-                var n = this.a / Math.Sqrt (con);
-                var ml = this.a * Proj4js.common.mlfn (this.e0, this.e1, this.e2, this.e3, lat);
+                con = 1.0 - Datum.es * Math.Pow (sin_phi, 2);
+                var n = Ellipsoid.a / Math.Sqrt (con);
+                var ml = Ellipsoid.a * ProjectionUtils.mlfn (e0, e1, e2, e3, lat);
 
-                x = this.k0 * n * al * (1.0 + als / 6.0 * (1.0 - t + c + als / 20.0 * (5.0 - 18.0 * t + Math.Pow (t, 2) + 72.0 * c - 58.0 * this.ep2))) + this.x0;
-                y = this.k0 * (ml - this.ml0 + n * tq * (als * (0.5 + als / 24.0 * (5.0 - t + 9.0 * c + 4.0 * Math.Pow (c, 2) + als / 30.0 * (61.0 - 58.0 * t + Math.pow (t, 2) + 600.0 * c - 330.0 * this.ep2))))) + this.y0;
-
+                x = k0.Value * n * al * (1.0 + als / 6.0 * (1.0 - t + c + als / 20.0 * (5.0 - 18.0 * t + Math.Pow (t, 2) + 72.0 * c - 58.0 * Datum.ep2))) + x0;
+                y = k0.Value * (ml - ml0 + n * tq * (als * (0.5 + als / 24.0 * (5.0 - t + 9.0 * c + 4.0 * Math.Pow (c, 2) + als / 30.0 * (61.0 - 58.0 * t + Math.Pow (t, 2) + 600.0 * c - 330.0 * Datum.ep2))))) + y0;
             }
 
-            p.x = x; p.y = y;
+            coords[0] = x; 
+            coords[1] = y;
         }
+
+        public override void Initialize ()
+        {
+            base.Initialize ();
+
+            e0 = ProjectionUtils.e0fn (Datum.es);
+            e1 = ProjectionUtils.e1fn (Datum.es);
+            e2 = ProjectionUtils.e2fn (Datum.es);
+            e3 = ProjectionUtils.e3fn (Datum.es);
+            ml0 = Ellipsoid.a * ProjectionUtils.mlfn (e0, e1, e2, e3, lat0);
+        }
+
+        private double e0, e1, e2, e3;
+        private double ml0;
     }
 }
