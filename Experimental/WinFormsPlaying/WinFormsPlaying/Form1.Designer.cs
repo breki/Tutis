ddef6c589584b3336d1eee5c2527a40517eb5ea6
Igor <226863+breki@users.noreply.github.com>
2012-02-25 10:18:51 +0100

    /

--- Experimental/WinFormsPlaying/WinFormsPlaying/Form1.Designer.cs
+++ Experimental/WinFormsPlaying/WinFormsPlaying/Form1.Designer.cs
@@ -28,107 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.brekiTabPanel1 = new WinFormsPlaying.BrekiTabsFlow();
-            this.tabButton4 = new WinFormsPlaying.TabButton(null);
-            this.tabButton2 = new WinFormsPlaying.TabButton(null);
-            this.closeButton1 = new WinFormsPlaying.CloseButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.AutoSize = true;
-            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(121, 82);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(22, 22);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button2.Location = new System.Drawing.Point(278, 92);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.button2.Size = new System.Drawing.Size(185, 35);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Map Sources";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // brekiTabPanel1
             // 
+            this.brekiTabPanel1.AutoSize = true;
+            this.brekiTabPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.brekiTabPanel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.brekiTabPanel1.Location = new System.Drawing.Point(231, 241);
             this.brekiTabPanel1.Name = "brekiTabPanel1";
-            this.brekiTabPanel1.Size = new System.Drawing.Size(505, 48);
+            this.brekiTabPanel1.Size = new System.Drawing.Size(0, 0);
             this.brekiTabPanel1.TabIndex = 7;
             // 
-            // tabButton4
+            // richTextBox1
             // 
-            this.tabButton4.AutoSize = true;
-            this.tabButton4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tabButton4.CanClose = false;
-            this.tabButton4.Location = new System.Drawing.Point(53, 133);
-            this.tabButton4.Margin = new System.Windows.Forms.Padding(0);
-            this.tabButton4.Name = "tabButton4";
-            this.tabButton4.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.tabButton4.Size = new System.Drawing.Size(107, 34);
-            this.tabButton4.TabIndex = 6;
-            this.tabButton4.Text = "tabButton4";
-            this.tabButton4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.tabButton4.UseCompatibleTextRendering = true;
-            this.tabButton4.UseVisualStyleBackColor = true;
-            // 
-            // tabButton2
-            // 
-            this.tabButton2.AutoSize = true;
-            this.tabButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tabButton2.Location = new System.Drawing.Point(535, 52);
-            this.tabButton2.Margin = new System.Windows.Forms.Padding(0);
-            this.tabButton2.Name = "tabButton2";
-            this.tabButton2.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.tabButton2.Size = new System.Drawing.Size(112, 34);
-            this.tabButton2.TabIndex = 4;
-            this.tabButton2.Text = "tabButton2";
-            this.tabButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.tabButton2.UseCompatibleTextRendering = true;
-            this.tabButton2.UseVisualStyleBackColor = true;
-            // 
-            // closeButton1
-            // 
-            this.closeButton1.AutoSize = true;
-            this.closeButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.closeButton1.BackColor = System.Drawing.Color.Transparent;
-            this.closeButton1.FlatAppearance.BorderSize = 0;
-            this.closeButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
-            this.closeButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.closeButton1.Image = ((System.Drawing.Image)(resources.GetObject("closeButton1.Image")));
-            this.closeButton1.Location = new System.Drawing.Point(463, 203);
-            this.closeButton1.Name = "closeButton1";
-            this.closeButton1.Size = new System.Drawing.Size(22, 22);
-            this.closeButton1.TabIndex = 3;
-            this.closeButton1.TabStop = false;
-            this.closeButton1.Text = null;
-            this.closeButton1.UseCompatibleTextRendering = true;
-            this.closeButton1.UseVisualStyleBackColor = true;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(806, 359);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "ddsd\nsdfsdf\ndsf\nsdf\nsdfsd\nfsd\nfsdfsd";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(806, 359);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.brekiTabPanel1);
-            this.Controls.Add(this.tabButton2);
-            this.Controls.Add(this.closeButton1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
@@ -140,12 +71,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private CloseButton closeButton1;
-        private TabButton tabButton2;
-        private TabButton tabButton4;
         private BrekiTabsFlow brekiTabPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
