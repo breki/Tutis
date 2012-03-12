5a2a0b76040101d1c76b403c2bff10b1237b6d9d
brejc <226863+breki@users.noreply.github.com>
2012-03-12 10:31:10 +0100

    Added step 8 to CleanCode.

---
+++ Projects/CleanCode/CleanCode/Step8/WeatherDataCollectorTests.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+
+namespace CleanCode.Step8
+{
+    public class WeatherDataCollectorTests
+    {
+        [Test]
+        public void AssertTemperatureIsRight()
+        {
+            int temperature = weatherService.GetTemp(weatherData, "Kredarica");
+            Assert.AreEqual(-11, temperature);
+        }
+
+        [Test]
+        public void AssertPressureTrendIsRight()
+        {
+            string pressureTrend = weatherService.GetPressure(weatherData, "Murska Sobota");
+            Assert.AreEqual("raste", pressureTrend);
+        }
+
+        [Test]
+        public void MakeSureAllRowsHaveBeenRead()
+        {
+            Assert.AreEqual(11, weatherData.RowsCount);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            FileTextFetcher textFetcher = new FileTextFetcher();
+            IWeatherDataReader weatherDataReader = new ArsoHtmlWeatherDataReader();
+            WeatherDataCollector weatherDataCollector = new WeatherDataCollector(textFetcher, weatherDataReader);
+            weatherData = weatherDataCollector.Parse("SampleData/sample.html");
+            weatherService = new WeatherService();
+        }
+
+        private WeatherData weatherData;
+        private WeatherService weatherService;
+    }
+}
