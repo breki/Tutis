78fc50418bc73062fe501ceedded11d3f078c305
Igor <Igor@Jazz.triera.net>
2012-07-03 15:09:24 +0200

    .

--- Experimental/WinFormsPlaying/WinFormsPlaying/Form1.Designer.cs
+++ Experimental/WinFormsPlaying/WinFormsPlaying/Form1.Designer.cs
@@ -28,61 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.brekiTabPanel1 = new WinFormsPlaying.BrekiTabsFlow();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(129, 227);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 21);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "label1";
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox2.Location = new System.Drawing.Point(308, 185);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.ReadOnly = true;
-            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.richTextBox2.Size = new System.Drawing.Size(306, 77);
-            this.richTextBox2.TabIndex = 10;
-            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
-            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
-            // 
-            // brekiTabPanel1
-            // 
-            this.brekiTabPanel1.AutoSize = true;
-            this.brekiTabPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.brekiTabPanel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.brekiTabPanel1.Location = new System.Drawing.Point(231, 241);
-            this.brekiTabPanel1.Name = "brekiTabPanel1";
-            this.brekiTabPanel1.Size = new System.Drawing.Size(0, 0);
-            this.brekiTabPanel1.TabIndex = 7;
-            // 
-            // richTextBox1
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(806, 359);
+            this.richTextBox1.Size = new System.Drawing.Size(534, 247);
             this.richTextBox1.TabIndex = 8;
             this.richTextBox1.Text = "ddsd\nsdfsdf\ndsf\nsdf\nsdfsd\nfsd\nfsdfsd";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(338, 210);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 31);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Later";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -93,26 +51,23 @@
             this.button3.TabIndex = 11;
             this.button3.Text = "Ignore";
             this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(534, 247);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.brekiTabPanel1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private BrekiTabsFlow brekiTabPanel1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button3;
     }
--- Experimental/WinFormsPlaying/WinFormsPlaying/Form1.cs
+++ Experimental/WinFormsPlaying/WinFormsPlaying/Form1.cs
@@ -8,29 +8,6 @@ namespace WinFormsPlaying
         public Form1()
         {
             InitializeComponent();
-
-            brekiTabPanel1.AddTab("Map", null, false);
-            brekiTabPanel1.AddTab("Release Notes", null, true);
-            brekiTabPanel1.AddTab("Rules Editor", null, true);
-            brekiTabPanel1.TabCloseButtonClick += new EventHandler<BrekiTabsFlowEventArgs>(brekiTabPanel1_TabCloseButtonClick);
-        }
-
-        private void brekiTabPanel1_TabButtonClick(object sender, BrekiTabsFlowEventArgs e)
-        {
-            brekiTabPanel1.ActivateTab(e.TabIndex);
-        }
-
-        void brekiTabPanel1_TabCloseButtonClick(object sender, BrekiTabsFlowEventArgs e)
-        {
-            brekiTabPanel1.RemoveTab(e.TabIndex);
-        }
-
-        private void richTextBox1_TextChanged (object sender, EventArgs e)
-        {
-        }
-
-        private void richTextBox2_TextChanged(object sender, EventArgs e)
-        {
         }
     }
 }
