3a5c919c7c5f0073395ba2306c553321e9c050e6
Igor <226863+breki@users.noreply.github.com>
2013-01-11 22:10:08 +0100

    Adobe Illustrator scripting investigation.

---
+++ Experimental/IllyPlaying/IllyPlaying/BasicTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Illustrator;
+using NUnit.Framework;
+
+namespace IllyPlaying
+{
+    public class BasicTests
+    {
+        [Test]
+        public void OpenAI()
+        {
+            Application app = new Application();
+            foreach (Document document1 in app.Documents)
+                document1.Close(AiSaveOptions.aiDoNotSaveChanges);
+
+            Document document = app.Open (
+                @"D:\MyStuff\Dropbox\Public\Pigment\H_Maribor_130102_180245.pdf", AiDocumentColorSpace.aiDocumentRGBColor, null);
+
+            Layer zeroLayer = document.ActiveLayer;
+
+            Debug.WriteLine("GraphItems: {0}", zeroLayer.GraphItems.Count);
+            Debug.WriteLine ("PathItems: {0}", zeroLayer.PathItems.Count);
+            Debug.WriteLine ("GroupItems: {0}", zeroLayer.GroupItems.Count);
+            Debug.WriteLine ("CompoundPathItems: {0}", zeroLayer.CompoundPathItems.Count);
+            Debug.WriteLine ("NonNativeItems: {0}", zeroLayer.NonNativeItems.Count);
+            Debug.WriteLine ("PageItems: {0}", zeroLayer.PageItems.Count);
+            Debug.WriteLine ("PlacedItems: {0}", zeroLayer.PlacedItems.Count);
+            Debug.WriteLine ("RasterItems: {0}", zeroLayer.RasterItems.Count);
+            Debug.WriteLine ("SymbolItems: {0}", zeroLayer.SymbolItems.Count);
+            Debug.WriteLine ("TextFrames: {0}", zeroLayer.TextFrames.Count);
+
+            Layer newLayer = document.Layers.Add();
+            newLayer.Name = "text layer";
+
+            List<TextFrame> textFrames = new List<TextFrame>();
+            foreach (TextFrame item in zeroLayer.TextFrames)
+                textFrames.Add(item);
+
+            foreach (TextFrame item in textFrames)
+            {
+                item.Move(newLayer, AiElementPlacement.aiPlaceAtEnd);
+                //Debug.WriteLine("Moved text '{0}'", item.Name, null);
+            }
+
+            Layer groupLayer = document.Layers.Add();
+            groupLayer.Name = "group layer";
+
+            List<GroupItem> groupItems = new List<GroupItem> ();
+            foreach (GroupItem item in zeroLayer.GroupItems)
+                groupItems.Add (item);
+
+            foreach (GroupItem item in groupItems)
+                item.Move (groupLayer, AiElementPlacement.aiPlaceAtEnd);
+
+            Debug.WriteLine ("zero.GroupItems: {0}", zeroLayer.GroupItems.Count);
+            Debug.WriteLine ("zero.TextFrames: {0}", zeroLayer.TextFrames.Count);
+            Debug.WriteLine ("newLayer.GroupItems: {0}", newLayer.GroupItems.Count);
+            Debug.WriteLine ("newLayer.TextFrames: {0}", newLayer.TextFrames.Count);
+            Debug.WriteLine ("groupLayer.GroupItems: {0}", groupLayer.GroupItems.Count);
+            Debug.WriteLine ("groupLayer.TextFrames: {0}", groupLayer.TextFrames.Count);
+
+            //foreach (PathItem pageItem in zeroLayer.PathItems)
+            //    pageItem.Move(newLayer, AiElementPlacement.aiPlaceAtEnd);
+
+            IllustratorSaveOptions options = new IllustratorSaveOptions();
+            options.FlattenOutput = AiOutputFlattening.aiPreserveAppearance;
+            options.FontSubsetThreshold = 100;
+            options.PDFCompatible = true;
+            options.Compatibility = AiCompatibility.aiIllustrator10;
+            options.Compressed = true;
+            document.SaveAs (@"D:\hg\Tutis\Experimental\IllyPlaying\IllyPlaying\bin\Debug\test.ai", options);
+
+            //Thread.Sleep(5000);
+            //app.Quit();
+        }         
+
+        //     doc.Export(@"F:/AI_Prog/2009Calendar.png",Illustrator.AiExportType.aiPNG24, null);  
+        // doc.Close(Illustrator.AiSaveOptions.aiDoNotSaveChanges); 
+    }
+}
