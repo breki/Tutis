3c08ef03d77c53c3156f7fd54ae709eacfcc4ffd
brejc <brejc@TERAN.hermes.si>
2013-03-06 10:26:53 +0100

    Implemented a simple W3C extended log file parser.

---
+++ Projects/Elf/Elf/ElfReader.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Elf
+{
+    public class ElfReader : IElfReader
+    {
+        public LogContents ReadLogFile(string fileName)
+        {
+            LogContents contents = new LogContents();
+
+            string[] lines = File.ReadAllLines(fileName);
+
+            foreach (string line in lines)
+                ParseLine(line, contents);
+
+            return contents;
+        }
+
+        private void ParseLine(string line, LogContents contents)
+        {
+            if (line.StartsWith("#"))
+                ParseDirective(line, contents);
+            else
+                ParseLogEntry(line, contents);
+        }
+
+        private void ParseDirective(string line, LogContents contents)
+        {
+            if (line.StartsWith("#Fields"))
+                ParseFields(line, contents);
+        }
+
+        private void ParseFields(string line, LogContents contents)
+        {
+            string fieldsSubstring = line.Substring("#Fields:".Length).Trim();
+            string[] fields = fieldsSubstring.Split(' ');
+            fieldsContext = new FieldsContext(fields);
+        }
+
+        private void ParseLogEntry(string line, LogContents contents)
+        {
+            string[] columns = line.Split(' ');
+
+            LogEntry entry = new LogEntry();
+            for (int i = 0; i < fieldsContext.Fields.Count; i++)
+            {
+                string field = fieldsContext.Fields[i];
+                string value = columns[i];
+
+                ProcessFieldValue(entry, field, value);
+            }
+
+            contents.AddLogEntry(entry);
+        }
+
+        private static void ProcessFieldValue(LogEntry entry, string field, string value)
+        {
+            switch (field)
+            {
+                case "date":
+                    entry.Date = DateTime.Parse(value, CultureInfo.InvariantCulture);
+                    break;
+                case "time":
+                    entry.Time = TimeSpan.Parse(value, CultureInfo.InvariantCulture);
+                    break;
+                case "c-ip":
+                    entry.ClientIp = ParseFieldValue(value);
+                    break;
+                case "cs-method":
+                    entry.Method = ParseFieldValue (value);
+                    break;
+                case "cs-uri-stem":
+                    entry.UriStem = ParseFieldValue (value);
+                    break;
+                case "cs-uri-query":
+                    entry.UriQuery = ParseFieldValue (value);
+                    break;
+                case "sc-status":
+                    entry.ServerStatus = ParseFieldValue (value);
+                    break;
+                case "cs(User-Agent)":
+                    entry.UserAgent = ParseFieldValue (value);
+                    break;
+                case "cs(Referrer)":
+                    entry.Referrer = ParseFieldValue (value);
+                    break;
+            }
+        }
+
+        private static string ParseFieldValue(string fieldValue)
+        {
+            if (fieldValue == "-")
+                return null;
+
+            return fieldValue;
+        }
+
+        private FieldsContext fieldsContext;
+    }
+}
---
+++ Projects/Elf/Elf/LogContents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Elf
+{
+    public class LogContents
+    {
+        public void AddLogEntry(LogEntry entry)
+        {
+            entries.Add(entry);
+        }
+
+        public IList<LogEntry> Entries
+        {
+            get { return entries; }
+        }
+
+        private List<LogEntry> entries = new List<LogEntry>();
+    }
+}
