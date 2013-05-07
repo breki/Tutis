742fc4544f3540133a7f4f7bf47e85bdc275047e
igor <igor@localhost>
2013-05-07 07:04:28 +0200

    Experimenting with SOP.

--- Experimental/SpatialitePlaying/SpatialitePlaying/TestOsmDataBulkInsertSession.cs
+++ Experimental/SpatialitePlaying/SpatialitePlaying/TestOsmDataBulkInsertSession.cs
@@ -26,33 +26,33 @@ namespace SpatialitePlaying
             if (readingPhase != ReadingPhase.ReadNodes)
                 throw new InvalidOperationException ();
 
-            collectedNodes.Add(node);
+            //collectedNodes.Add(node);
 
-            if (collectedNodes.Count >= 100000)
-            {
-                DBBulkProcessor.BulkUpdate(
-                    SqliteHelper.SqliteProviderFactory,
-                    storage.DbConnection,
-                    "SELECT node_id FROM nodes",
-                    "UPDATE nodes SET x=@p1, y=@p2 WHERE node_id=@p3",
-                    dt =>
-                    {
-                        foreach (OsmNode nodeToDb in collectedNodes)
-                        {
-                            DataRow row = dt.NewRow ();
-                            row[0] = nodeToDb.X;
-                            row[1] = nodeToDb.Y;
-                            row[2] = nodeToDb.ObjectId;
-                            dt.Rows.Add (row);
-                        }
-                    });
+            //if (collectedNodes.Count >= 100000)
+            //{
+            //    DBBulkProcessor.BulkUpdate(
+            //        SqliteHelper.SqliteProviderFactory,
+            //        storage.DbConnection,
+            //        "SELECT node_id FROM nodes",
+            //        "UPDATE nodes SET x=@p1, y=@p2 WHERE node_id=@p3",
+            //        dt =>
+            //        {
+            //            foreach (OsmNode nodeToDb in collectedNodes)
+            //            {
+            //                DataRow row = dt.NewRow ();
+            //                row[0] = nodeToDb.X;
+            //                row[1] = nodeToDb.Y;
+            //                row[2] = nodeToDb.ObjectId;
+            //                dt.Rows.Add (row);
+            //            }
+            //        });
 
-                nodesCounter += collectedNodes.Count;
-                Console.WriteLine ("Nodes updated: {0} ({1} nodes/s)", nodesCounter, collectedNodes.Count / stopwatch.Elapsed.TotalSeconds);
-                stopwatch.Restart ();
+            //    nodesCounter += collectedNodes.Count;
+            //    Console.WriteLine ("Nodes updated: {0} ({1} nodes/s)", nodesCounter, collectedNodes.Count / stopwatch.Elapsed.TotalSeconds);
+            //    stopwatch.Restart ();
 
-                collectedNodes.Clear ();
-            }
+            //    collectedNodes.Clear ();
+            //}
         }
 
         public void AddNote(string noteType, string note)
@@ -72,30 +72,22 @@ namespace SpatialitePlaying
                 return;
 
             storage.UsedWays.Add(way);
-            collectedNodeIds.AddRange(way.Nodes);
 
-            if (collectedNodeIds.Count >= 100000)
+            foreach (long nodeId in way.Nodes)
             {
-                DBBulkProcessor.BulkInsert(
-                    SqliteHelper.SqliteProviderFactory,
-                    storage.DbConnection,
-                    "SELECT node_id FROM nodes",
-                    "INSERT OR IGNORE INTO nodes (node_id) VALUES (@param1)",
-                    dt =>
-                        {
-                            foreach (long nodeId in collectedNodeIds)
-                            {
-                                DataRow row = dt.NewRow ();
-                                row[0] = nodeId;
-                                dt.Rows.Add (row);                                
-                            }
-                        });
+                storage.NodesDict.Add(nodeId, true);
+                cachedNodesCount++;
+            }
 
-                nodesCounter += collectedNodeIds.Count;
-                Console.WriteLine("Nodes indexed: {0} ({1} nodes/s)", nodesCounter, collectedNodeIds.Count / stopwatch.Elapsed.TotalSeconds);
+            if (cachedNodesCount >= 100000)
+            {
+                storage.NodesStorage.Commit();
+
+                nodesCounter += cachedNodesCount;
+                Console.WriteLine("Nodes indexed: {0} ({1} nodes/s)", nodesCounter, cachedNodesCount / stopwatch.Elapsed.TotalSeconds);
                 stopwatch.Restart();
 
-                collectedNodeIds.Clear();
+                cachedNodesCount = 0;
             }
         }
 
@@ -123,7 +115,7 @@ namespace SpatialitePlaying
         private bool disposed;
         private readonly TestOsmDataStorage storage;
         private readonly ReadingPhase readingPhase;
-        private List<long> collectedNodeIds = new List<long>();
+        private long cachedNodesCount;
         private List<OsmNode> collectedNodes = new List<OsmNode>();
         private long nodesCounter;
         private Stopwatch stopwatch = new Stopwatch();
