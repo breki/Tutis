c0b1917a56b9200a888154f57c58150d7cd096d3
brejc <226863+breki@users.noreply.github.com>
2013-05-07 19:32:07 +0200

    Working on the block writing of nodes.

---
+++ Experimental/SpatialitePlaying/SpatialitePlaying/OsmNodesBinaryRecorder.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Brejc.Common.FileSystem;
+using Brejc.OsmLibrary;
+
+namespace SpatialitePlaying
+{
+    public class OsmNodesBinaryRecorder : IOsmDataStorage, IOsmDataBulkInsertSession
+    {
+        public OsmNodesBinaryRecorder(IFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public string AttributionText { get; set; }
+
+        public IOsmDataBulkInsertSession StartBulkInsertSession (bool threadSafe)
+        {
+            stream = fileSystem.OpenFileToWrite("nodes.dat");
+            writer = new BinaryWriter(stream);
+
+            return this;
+        }
+
+        public void EndBulkInsertSession ()
+        {
+        }
+
+        public void Dispose ()
+        {
+        }
+
+        public void AddNode (OsmNode node)
+        {
+            if (nodesInBlockCounter%100 == 0)
+            {
+                NodesBlock block = new NodesBlock(node.ObjectId, stream.Position);
+                blocks.Add(block);
+                nodesInBlockCounter = 0;
+            }
+
+            writer.Write(node.ObjectId);
+            writer.Write(node.X);
+            writer.Write(node.Y);
+
+            nodesInBlockCounter++;
+        }
+
+        public void AddWay (OsmWay way)
+        {
+        }
+
+        public void AddRelation (OsmRelation relation)
+        {
+        }
+
+        public void AddBoundingBox (OsmBoundingBox box)
+        {
+        }
+
+        public void AddNote (string noteType, string note)
+        {
+        }
+
+        private readonly IFileSystem fileSystem;
+        private Stream stream;
+        private BinaryWriter writer;
+        private List<NodesBlock> blocks = new List<NodesBlock>();
+        private int nodesInBlockCounter;
+    }
+}
