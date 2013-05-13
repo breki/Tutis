dc3c4781382f8966b99eaaee42bfba15259c3cff
igor <igor@localhost>
2013-05-13 21:05:36 +0200

    Some prototype rendering of data from the storage.

--- Experimental/SpatialitePlaying/SpatialitePlaying/NodeIndexBuilding1/WaysIndexing/WayData.cs
+++ Experimental/SpatialitePlaying/SpatialitePlaying/NodeIndexBuilding1/WaysIndexing/WayData.cs
@@ -1,4 +1,8 @@
-﻿using SpatialitePlaying.NodeIndexBuilding1.OsmObjectIndexing;
+﻿using System;
+using System.IO;
+using Brejc.Common.BinaryProcessing;
+using Brejc.Geometry;
+using SpatialitePlaying.NodeIndexBuilding1.OsmObjectIndexing;
 
 namespace SpatialitePlaying.NodeIndexBuilding1.WaysIndexing
 {
@@ -30,6 +34,44 @@ namespace SpatialitePlaying.NodeIndexBuilding1.WaysIndexing
         public byte[] PointsBlob
         {
             get { return pointsBlob; }
+        }
+
+        public IPointD2List GetPointsList ()
+        {
+            double resolution = 1000 * OsmObjectIndexingHelper.Nanodegree;
+            bool duplicateFirstPointAsLast = true;
+
+            using (MemoryStream stream = new MemoryStream (pointsBlob))
+            using (BinaryReader binaryReader = new BinaryReader (stream))
+            {
+                int pointsInDbCount = (int)binaryReader.ReadVarintUInt64 ();
+                int totalPointsCount = pointsInDbCount + (duplicateFirstPointAsLast ? 1 : 0);
+                PointD2Array points = new PointD2Array (totalPointsCount);
+
+                long lxi = 0;
+                long lyi = 0;
+
+                for (int i = 0; i < pointsInDbCount; i++)
+                {
+                    long xi = binaryReader.ReadVarintInt64 ();
+                    long yi = binaryReader.ReadVarintInt64 ();
+
+                    if (xi == 0 && yi == 0)
+                        throw new InvalidOperationException ("Skip duplicate points");
+
+                    double x = (xi + lxi) * resolution;
+                    double y = (yi + lyi) * resolution;
+                    points.SetPoint (i, x, y);
+
+                    lxi = xi + lxi;
+                    lyi = yi + lyi;
+                }
+
+                if (duplicateFirstPointAsLast)
+                    points.SetPoint (totalPointsCount - 1, points.GetPointD2 (0));
+
+                return points;
+            }
         }
 
         private long wayId;
