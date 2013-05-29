e898bb4e835fa077e7507d13bbb8c32399813782
brejc <brejc@TERAN.hermes.si>
2013-05-29 10:10:27 +0200

    FinanceReport: implemented DropBoxBackupStorage.

---
+++ Projects/FinanceReport/FinanceReport/Tests/BackupStorageTests.cs
@@ -0,0 +1,16 @@
+﻿using FinanceReport.BackupStorage;
+using NUnit.Framework;
+
+namespace FinanceReport.Tests
+{
+    public class BackupStorageTests
+    {
+        [Test]
+        public void Test()
+        {
+            IBackupStorage storage = new DropBoxBackupStorage ();
+            string file = storage.FindLatestBackupFile();
+            Assert.IsNotNull(file);
+        } 
+    }
+}
