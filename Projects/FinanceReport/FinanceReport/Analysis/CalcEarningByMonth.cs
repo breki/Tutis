4a03d25c2440925b6478a26239f6609a05475043
igor <igor@localhost>
2013-07-01 20:29:08 +0200

    More work on reports.

--- Projects/FinanceReport/FinanceReport/Analysis/CalcEarningByMonth.cs
+++ Projects/FinanceReport/FinanceReport/Analysis/CalcEarningByMonth.cs
@@ -7,6 +7,9 @@ namespace FinanceReport.Analysis
     {
         public static AmountByRange Calc(Database db)
         {
+            DateTime now = DateTime.Now;
+            int nowMonthIndex = GetMonthIndex (now) - GetMonthIndex (new DateTime (2011, 1, 1));
+
             DatabaseTable table = db.Tables["transactions"];
 
             AmountByRange data = new AmountByRange ();
@@ -19,8 +22,13 @@ namespace FinanceReport.Analysis
                     continue;
 
                 if (tx.ToAccountId == 0 && tx.FromAmount >= 0)
-                    data.AddAmount (
-                        GetMonthIndex (tx.Date) - GetMonthIndex (new DateTime (2011, 1, 1)), tx.FromAmount);
+                {
+                    int monthIndex = GetMonthIndex(tx.Date) - GetMonthIndex(new DateTime(2011, 1, 1));
+                    if (nowMonthIndex - monthIndex > 13)
+                        continue;
+
+                    data.AddAmount (monthIndex, tx.FromAmount);
+                }
             }
 
             return data;
