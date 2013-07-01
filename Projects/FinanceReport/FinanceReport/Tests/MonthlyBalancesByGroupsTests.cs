69aa4be55d336ef76d9be5fe46d3959545daea4e
brejc <226863+breki@users.noreply.github.com>
2013-07-01 12:44:23 +0200

    FinanceReport: Working on a new report.

---
+++ Projects/FinanceReport/FinanceReport/Tests/MonthlyBalancesByGroupsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using FinanceReport.Analysis;
+using FinanceReport.DataModel;
+using NUnit.Framework;
+
+namespace FinanceReport.Tests
+{
+    public class MonthlyBalancesByGroupsTests
+    {
+        [Test]
+        public void Test()
+        {
+            Database db = FinancistoReadingTests.FetchLatestFinancistoData();
+            DatabaseTable categoriesTable = db.Tables["category"];
+
+            CalcMonthlyBalancesByGroups calc = new CalcMonthlyBalancesByGroups(db);
+            calc
+                .AddGroup("bencin", 2)
+                .AddGroup("avto - ostali stroški", 3, 16, 17, 18, 6, 30)
+                .AddGroup("hrana in nakupi", 4, 5, 10)
+                .AddGroup("kredit", 19)
+                .AddGroup("stanovanje", 12, 34, 35)
+                .AddGroup("ostalo");
+
+            foreach (TableRow row in categoriesTable.Rows)
+            {
+                Debug.WriteLine(row);
+            }
+
+            CategoriesRangesAmounts data = calc.Calc();
+        } 
+    }
+}
