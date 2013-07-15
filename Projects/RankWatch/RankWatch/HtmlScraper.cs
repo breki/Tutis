79795e20d4a219cba0a1057df2e8e972d5c9939a
brejc <226863+breki@users.noreply.github.com>
2013-07-15 14:46:25 +0200

    Started implementing RankWatch, a Google SERP analyzer.

---
+++ Projects/RankWatch/RankWatch/HtmlScraper.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Net;
+
+namespace RankWatch
+{
+    public class HtmlScraper
+    {
+        public void LoadFile(string fileName)
+        {
+            html = File.ReadAllText (@"..\..\..\data\sample-serp.html");
+            index = 0;
+        }
+
+        public void Download(string url, string userAgent)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add(HttpRequestHeader.UserAgent, userAgent);
+                html = wc.DownloadString(url);
+                index = 0;
+            }
+        }
+
+        public bool FindNext(string text)
+        {
+            int pos = html.IndexOf(text, index, System.StringComparison.Ordinal);
+            if (pos == -1)
+                return false;
+
+            index = pos + text.Length;
+            return true;
+        }
+
+        public string ExtractUntil(string text)
+        {
+            int pos = html.IndexOf(text, index, System.StringComparison.Ordinal);
+
+            string extract = html.Substring(index, pos - index);
+            index = pos + text.Length;
+
+            return extract;
+        }
+
+        public void SaveHtml (string fileName)
+        {
+            File.WriteAllText(fileName, html);
+        }
+
+        private string html;
+        private int index;
+    }
+}
---
+++ Projects/RankWatch/RankWatch/Tests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace RankWatch
+{
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            GoogleSearchRequestBuilder requestBuilder = new GoogleSearchRequestBuilder();
+            HtmlScraper scraper = new HtmlScraper();
+            //scraper.LoadFile(@"..\..\..\data\sample-serp.html");
+            //scraper.Download (requestBuilder.ConstructUrl("vector map of USA", 0), requestBuilder.GetUserAgent());
+
+            // http://www.google.si/search?gs_rn=19&gs_ri=psy-ab&pq=vector%20map%20of%20usa&cp=17&gs_id=g&xhr=t&q=vector+map+of+usa&es_nrs=true&pf=p&sclient=psy-ab&oq=vector+map+of+usa&gs_l=&pbx=1&biw=1600&bih=1083&cad=cbv&sei=q9_jUayzL8vEtAbcjIHIAQ
+            // http://www.google.si/search?q=vector+map+of+usa&safe=off&ei=rt_jUfWEHMnNON6WgZgP&sqi=2&start=10&sa=N&biw=1600&bih=1083
+
+            //scraper.SaveHtml(@"..\..\..\data\sample-serp.html");
+
+            int pageNumber = 0;
+            int resultCount = 0;
+
+            bool endSearch = false;
+            while (!endSearch)
+            {
+                scraper.Download (
+                    requestBuilder.ConstructUrl ("vector map of europe", pageNumber), 
+                    requestBuilder.GetUserAgent ());
+                scraper.SaveHtml (@"output.html");
+
+                while (scraper.FindNext("<div class=\"kv kva"))
+                {
+                }
+
+                if (!scraper.FindNext("<div class=\"f kv"))
+                    break;
+
+                while (true)
+                {
+                    // <div class="kv kva ads-visurl">
+                    // <div class="f kv" style="white-space: nowrap;">
+                    if (!scraper.FindNext("<cite"))
+                        break;
+
+                    if (!scraper.FindNext(">"))
+                        throw new InvalidOperationException("BUG 1");
+
+                    string resultUrl = scraper.ExtractUntil("</cite>");
+                    resultUrl = resultUrl.Replace("<b>", string.Empty);
+                    resultUrl = resultUrl.Replace("</b>", string.Empty);
+
+                    Debug.WriteLine("{0}: {1}", resultCount + 1, resultUrl);
+
+                    resultCount++;
+
+                    if (resultUrl.StartsWith("scalablemaps.com"))
+                    {
+                        endSearch = true;
+                        break;
+                    }
+                }
+
+                if (!endSearch)
+                    requestBuilder.WaitForNextPageQuery();
+
+                pageNumber++;
+            }
+        }
+    }
+}
