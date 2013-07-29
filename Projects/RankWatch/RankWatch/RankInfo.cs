95bd56a609d272b42e27ec9959d6f8d30986606a
brejc <226863+breki@users.noreply.github.com>
2013-07-29 09:24:09 +0200

    Extracted ranking code into RankFinder. It works now. The next step is to implement a framework for storing the ranks somewhere.

---
+++ Projects/RankWatch/RankWatch/RankInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace RankWatch
+{
+    public class RankInfo
+    {
+        public RankInfo(string searchKeyword)
+        {
+            this.searchKeyword = searchKeyword;
+        }
+
+        public string SearchKeyword
+        {
+            get { return searchKeyword; }
+        }
+
+        public IList<int> Ranks
+        {
+            get { return ranks; }
+        }
+
+        private string searchKeyword;
+        private List<int> ranks = new List<int>();
+    }
+}
