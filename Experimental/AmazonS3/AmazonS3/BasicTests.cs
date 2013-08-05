d67ccb60b34a395c558fbd52b0085d98829934dc
brejc <brejc@TERAN.hermes.si>
2013-08-05 11:08:45 +0200

    Some more playing.

--- Experimental/AmazonS3/AmazonS3/BasicTests.cs
+++ Experimental/AmazonS3/AmazonS3/BasicTests.cs
@@ -24,19 +24,25 @@ namespace AmazonS3
 
             using (TransferUtility utility = new TransferUtility(accessKeyID, secretAccessKey, config))
             {
-                Stopwatch stopwatch = new Stopwatch();
-                stopwatch.Start();
+                //Stopwatch stopwatch = new Stopwatch();
+                //stopwatch.Start();
 
-                string fileName = @"D:\MyStuff\Google Drive\ScalableMaps\map-products\china-natural-bg-low.pdf";
+                //string fileName = @"D:\MyStuff\Google Drive\ScalableMaps\map-products\china-natural-bg-low.pdf";
 
-                //TransferUtilityUploadRequest request = new TransferUtilityUploadRequest();
-                utility.Upload(fileName, bucketName);
+                ////TransferUtilityUploadRequest request = new TransferUtilityUploadRequest();
+                //utility.Upload(fileName, bucketName);
 
-                Console.WriteLine(
-                    "Uploaded in {0} ms ({1} KB/s)", 
-                    stopwatch.ElapsedMilliseconds,
-                    new FileInfo(fileName).Length / 1024.0 / (stopwatch.ElapsedMilliseconds / 1000));
+                //Console.WriteLine(
+                //    "Uploaded in {0} ms ({1} KB/s)", 
+                //    stopwatch.ElapsedMilliseconds,
+                //    new FileInfo(fileName).Length / 1024.0 / (stopwatch.ElapsedMilliseconds / 1000));
+
+                utility.Download (@"d:\brisi\china-natural-bg-low.pdf", bucketName, "china-natural-bg-low.pdf");
             }
+
+            // http://stackoverflow.com/questions/9920804/how-to-list-all-objects-in-amazon-s3-bucket
+            // http://stackoverflow.com/questions/1161022/storing-my-amazon-credentials-in-c-sharp-desktop-app?rq=1
+            // http://stackoverflow.com/questions/1856988/how-to-restrict-amazon-s3-api-access
         }
     }
 }
