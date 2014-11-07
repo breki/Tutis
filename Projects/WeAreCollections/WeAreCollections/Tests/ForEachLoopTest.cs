811adbc79ad373b0d39c353c892c96084f2755ad
igor <226863+breki@users.noreply.github.com>
2014-11-07 11:21:25 +0100

    Added List<> for vs foreach performance tests. Turns out there isn't a noticable difference.

---
+++ Projects/WeAreCollections/WeAreCollections/Tests/ForEachLoopTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeAreCollections.Tests
+{
+    public class ForEachLoopTest : IPerformanceTest
+    {
+        public const int ListSize = 100000000;
+
+        public int SuggestedTestStepsCount
+        {
+            get { return 10; }
+        }
+
+        public int Sum
+        {
+            get { return sum; }
+        }
+
+        public string GetTestDescription(int expectedTestStepsCount)
+        {
+            return "Runs foreach loop on a List<>";
+        }
+
+        public void Initialize(Random rnd, int expectedTestStepsCount)
+        {
+            list = new List<int>(ListSize);
+
+            for (int i = 0; i < ListSize; i++)
+                list.Add(rnd.Next());
+        }
+
+        public void ExecuteStep(int step)
+        {
+            sum = 0;
+            foreach (int item in list)
+                sum += item;
+        }
+
+        public void AssertValidity()
+        {
+        }
+
+        private List<int> list;
+        private int sum;
+    }
+}
