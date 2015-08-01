61e3edfe65db16cbdba520d3008d076f51ba9335
igor <igor@localhost>
2015-08-01 08:11:55 +0200

    Started implementing HtmlGenerator.

---
+++ Projects/Freude/Freude.Tests/HtmlGeneratingTests/BasicGeneratingTests.cs
@@ -0,0 +1,64 @@
+﻿using Freude.DocModel;
+using Freude.HtmlGenerating;
+using NUnit.Framework;
+
+namespace Freude.Tests.HtmlGeneratingTests
+{
+    public class BasicGeneratingTests
+    {
+        [Test]
+        public void SimpleParagraph()
+        {
+            ParagraphElement par = new ParagraphElement();
+            par.Children.Add(new TextElement("this is text"));
+            doc.Children.Add(par);
+
+            string html = generator.GenerateHtml(doc);
+            Assert.AreEqual("<p>this is text</p>", html);
+        }
+
+        [Test]
+        public void EscapeText()
+        {
+            ParagraphElement par = new ParagraphElement();
+            par.Children.Add(new TextElement("this is text < & >"));
+            doc.Children.Add(par);
+
+            string html = generator.GenerateHtml(doc);
+            Assert.AreEqual ("<p>this is text &lt; &amp; &gt;</p>", html);
+        }
+
+        [Test]
+        public void ParagraphWithImage()
+        {
+            ParagraphElement par = new ParagraphElement();
+            par.Children.Add(new TextElement("text before"));
+            par.Children.Add (new ImageElement ("http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif"));
+            par.Children.Add(new TextElement("text after"));
+            doc.Children.Add(par);
+
+            string html = generator.GenerateHtml(doc);
+            Assert.AreEqual ("<p>text before<img src=\"http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif\" />text after</p>", html);
+        }
+
+        [Test]
+        public void HeaderParagraph ()
+        {
+            HeaderElement el = new HeaderElement("header", 3);
+            doc.Children.Add (el);
+
+            string html = generator.GenerateHtml (doc);
+            Assert.AreEqual ("<h3>header</h3>", html);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            generator = new HtmlGenerator();
+            doc = new DocumentDef ();
+        }
+
+        private HtmlGenerator generator;
+        private DocumentDef doc;
+    }
+}
---
+++ Projects/Freude/Freude/Commands/BuildCommand.cs
@@ -0,0 +1,37 @@
+﻿using Brejc.Common.Console;
+
+namespace Freude.Commands
+{
+    public class BuildCommand : StandardConsoleCommandBase
+    {
+        public BuildCommand ()
+        {
+            AddArg ("template ID", "map template to use").Value ((x, env) => templateId = x);
+            AddArg ("theme ID", "map theme to use. If not specified, all themes will be used").Value ((x, env) => themeId = x).IsOptional ();
+            AddSwitch ("downloadOsm", "forces re-downloading of OSM data", (x, env) => forceOsmRedownload = x).Alias ("dlosm");
+            AddSwitch ("recreateDB", "forces recreation of geo DB", (x, env) => forceRecreateDb = x).Alias ("rcrdb");
+            AddSwitch ("debug", "switches on the debug rendering mode", (x, env) => debugMode = x);
+        }
+
+        public override string CommandId
+        {
+            get { return "build"; }
+        }
+
+        public override object Description
+        {
+            get { return "build the site"; }
+        }
+
+        public override int Execute (IConsoleEnvironment env)
+        {
+            return 0;
+        }
+
+        private string templateId;
+        private string themeId;
+        private bool forceOsmRedownload;
+        private bool forceRecreateDb;
+        private bool debugMode;
+    }
+}
