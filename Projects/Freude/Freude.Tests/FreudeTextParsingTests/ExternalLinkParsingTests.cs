83c2c3ad087ef622d5b544bad7d29848243837de
igor <igor@localhost>
2015-11-11 06:21:25 +0100

    Implemented support for external link descriptions.

--- Projects/Freude/Freude.Tests/FreudeTextParsingTests/ExternalLinkParsingTests.cs
+++ Projects/Freude/Freude.Tests/FreudeTextParsingTests/ExternalLinkParsingTests.cs
@@ -32,6 +32,20 @@ namespace Freude.Tests.FreudeTextParsingTests
             Assert.AreEqual (new Uri("http://google.com"), link.Url);
         }
         
+        [Test]
+        public void WithDescription()
+        {
+            fixture.Parse ("[ http://google.com   Google Website ]")
+                .AssertNoErrors ()
+                .AssertChildCount (1);
+
+            var par = fixture.AssertElement<ParagraphElement> (0);
+            Assert.AreEqual (1, par.ChildrenCount);
+            var link = fixture.AssertElement<ExternalLinkElement> (par, 0);
+            Assert.AreEqual (new Uri("http://google.com"), link.Url);
+            Assert.AreEqual ("Google Website", link.LinkDescription);
+        }
+        
         [SetUp]
         public void Setup ()
         {
