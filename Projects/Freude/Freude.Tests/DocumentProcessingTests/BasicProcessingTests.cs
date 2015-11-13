abaf90d79f8fc59f6795e052380c17b07d29cf54
igor <igor@localhost>
2015-11-13 10:47:02 +0100

    Introducing ProcessingFixture.

--- Projects/Freude/Freude.Tests/DocumentProcessingTests/BasicProcessingTests.cs
+++ Projects/Freude/Freude.Tests/DocumentProcessingTests/BasicProcessingTests.cs
@@ -1,5 +1,4 @@
-﻿using Freude.Tests.FreudeTextParsingTests;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Freude.Tests.DocumentProcessingTests
 {
@@ -8,34 +7,22 @@ namespace Freude.Tests.DocumentProcessingTests
         [Test]
         public void SimpleParagraph()
         {
-            fixture.Parse(@"This is a '''paragraph'''");
-
-            using (TestDocumentProcessor processor = new TestDocumentProcessor())
-            {
-                processor.ProcessDocument(fixture.Doc);
-                string result = processor.ResultHtml;
-                Assert.AreEqual (
+            fixture.ProcessText(
+                @"This is a '''paragraph'''",
 @"<body>
   <p>This is a <strong>paragraph</strong></p>
-</body>", 
-                    result);
-            }
+</body>");
         }
 
         [Test]
         public void Headings()
         {
-            fixture.Parse(@"= Title =
+            fixture.ProcessText(
+                @"= Title =
 
 == 1. Heading ==
 === 1.1 Heading ===
-This is a '''paragraph'''");
-
-            using (TestDocumentProcessor processor = new TestDocumentProcessor())
-            {
-                processor.ProcessDocument(fixture.Doc);
-                string result = processor.ResultHtml;
-                Assert.AreEqual (
+This is a '''paragraph'''",
 @"<body>
   <h1>
     Title
@@ -44,25 +31,18 @@ This is a '''paragraph'''");
   </h2><h3>
     1.1 Heading
   </h3><p>This is a <strong>paragraph</strong></p>
-</body>", 
-                    result);
-            }
+</body>");
         }
 
         [Test]
         public void HeadingWithAnchor()
         {
-            fixture.Parse(@"= Title =
+            fixture.ProcessText(
+                @"= Title =
 
 == 1. Heading ==
 === 1.1 Heading ===
-This is a '''paragraph'''");
-
-            using (TestDocumentProcessor processor = new TestDocumentProcessor())
-            {
-                processor.ProcessDocument(fixture.Doc);
-                string result = processor.ResultHtml;
-                Assert.AreEqual (
+This is a '''paragraph'''",
 @"<body>
   <h1>
     Title
@@ -71,91 +51,61 @@ This is a '''paragraph'''");
   </h2><h3>
     1.1 Heading
   </h3><p>This is a <strong>paragraph</strong></p>
-</body>", 
-                    result);
-            }
+</body>");
         }
 
         [Test]
         public void InternalLink()
         {
-            fixture.Parse (@"This is a [[Main Page|link to somewhere]]");
-
-            using (TestDocumentProcessor processor = new TestDocumentProcessor ())
-            {
-                processor.ProcessDocument (fixture.Doc);
-                string result = processor.ResultHtml;
-                Assert.AreEqual (
+            fixture.ProcessText(
+                @"This is a [[Main Page|link to somewhere]]",
 @"<body>
   <p>This is a <a href=""http://google.com/Main%20Page"">link to somewhere</a></p>
-</body>", 
-                    result);
-            }
+</body>");
         }
 
         [Test]
         public void InternalLinkNoDescription()
         {
-            fixture.Parse (@"This is a [[Main Page]]");
-
-            using (TestDocumentProcessor processor = new TestDocumentProcessor ())
-            {
-                processor.ProcessDocument (fixture.Doc);
-                string result = processor.ResultHtml;
-                Assert.AreEqual (
+            fixture.ProcessText(
+                @"This is a [[Main Page]]",
 @"<body>
   <p>This is a <a href=""http://google.com/Main%20Page"">Main Page</a></p>
-</body>", 
-                    result);
-            }
+</body>");
         }
 
         [Test]
         public void ExternalLink ()
         {
-            fixture.Parse (@"This is a [http://google.com link to somewhere]]");
-
-            using (TestDocumentProcessor processor = new TestDocumentProcessor ())
-            {
-                processor.ProcessDocument (fixture.Doc);
-                string result = processor.ResultHtml;
-                Assert.AreEqual (
+            fixture.ProcessText(
+                @"This is a [http://google.com link to somewhere]",
 @"<body>
   <p>This is a <a href=""http://google.com/"">link to somewhere</a></p>
-</body>", 
-                    result);
-            }
+</body>");
         }
 
         [Test]
         public void NumberedList()
         {
-            fixture.Parse (@"# item 1
+            fixture.ProcessText(
+                @"# item 1
 # item 2
 # item 3
 
-# item 4");
-
-            using (TestDocumentProcessor processor = new TestDocumentProcessor ())
-            {
-                processor.ProcessDocument (fixture.Doc);
-                string result = processor.ResultHtml;
-                Assert.AreEqual (
+# item 4",
 @"<body>
   <ol>
     <li>item 1</li><li>item 2</li><li>item 3</li><li>item 4</li>
   </ol>
-</body>",
-                    result);
-            }
+</body>");
         }
 
         [SetUp]
         public void Setup()
         {
-            fixture = new ParsingFixture();
+            fixture = new ProcessingFixture ();
         }
 
-        private ParsingFixture fixture;
+        private ProcessingFixture fixture;
     }
 }
---
+++ Projects/Freude/Freude.Tests/DocumentProcessingTests/ProcessingFixture.cs
@@ -0,0 +1,20 @@
+﻿using Freude.Tests.FreudeTextParsingTests;
+using NUnit.Framework;
+
+namespace Freude.Tests.DocumentProcessingTests
+{
+    public class ProcessingFixture : ParsingFixture
+    {
+        public void ProcessText(string text, string expectedResult)
+        {
+            Parse(text).AssertNoErrors();
+
+            using (TestDocumentProcessor processor = new TestDocumentProcessor ())
+            {
+                processor.ProcessDocument (Doc);
+                string result = processor.ResultHtml;
+                Assert.AreEqual (expectedResult, result);
+            }
+        }
+    }
+}
