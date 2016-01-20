adf51d6418a7050184949196461dbe2f966997dc
igor <igor@localhost>
2016-01-20 10:21:33 +0100

    Implemented installing of a self-signed certificate in the cert store.

---
+++ Experimental/SelfSignedHttpsListener/SelfSignedHttpsListener/CertificateStoreInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SelfSignedHttpsListener
+{
+    public class CertificateStoreInstaller
+    {
+        public void InstallCertificate(X509Certificate2 certificate)
+        {
+            X509Store store = new X509Store (StoreName.My, StoreLocation.LocalMachine);
+            store.Open (OpenFlags.ReadWrite);
+            store.Add (certificate);
+            store.Close (); 
+        }
+    }
+}
---
+++ Experimental/SelfSignedHttpsListener/SelfSignedHttpsListener/Tests/CertificateToolsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+
+namespace SelfSignedHttpsListener.Tests
+{
+    public class CertificateToolsTests
+    {
+        [Test]
+        public void SelfSignedCertificateGeneratorTest()
+        {
+            SelfSignedCertificateGenerator generator = new SelfSignedCertificateGenerator();
+            X509Certificate2 cert = generator.GenerateCertificate();
+            Assert.IsNotNull(cert);
+        }
+
+        [Test]
+        public void InstallCertificateInStore()
+        {
+            SelfSignedCertificateGenerator generator = new SelfSignedCertificateGenerator ();
+            X509Certificate2 cert = generator.GenerateCertificate ();
+    
+            CertificateStoreInstaller installer = new CertificateStoreInstaller();
+            installer.InstallCertificate(cert);
+        }
+    }
+}
