1502527eb5bdc60d87c6f0f6d8bb457932767e49
igor <igor@localhost>
2016-09-06 10:14:10 +0200

    Started working on CalculatorTdd solution, an example on how to do TDD.

---
+++ Experimental/CalculatorTdd/CalculatorTdd/Calculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CalculatorTdd
+{
+    public class Calculator
+    {
+        public Calculator(IDisplay display)
+        {
+            this.display = display;
+        }
+
+        public void Init()
+        {
+            inClearedMode = true;
+            currentlyDisplayedString = "0";
+            UpdateDisplay();
+        }
+
+        public void HandleKeyPress(Key key)
+        {
+            switch (key)
+            {
+                case Key.N0:
+                case Key.N1:
+                case Key.N2:
+                case Key.N3:
+                case Key.N4:
+                case Key.N5:
+                case Key.N6:
+                case Key.N7:
+                case Key.N8:
+                case Key.N9:
+                {
+                    HandleNumericKeyPress(key);
+                    return;
+                }
+
+                case Key.Dot:
+                case Key.Add:
+                case Key.Sub:
+                case Key.Mul:
+                case Key.Div:
+                case Key.Equals:
+                case Key.C:
+                case Key.MRC:
+                case Key.MPlus:
+                case Key.MMinus:
+                default:
+                    throw new NotImplementedException("todo next:");
+            }
+        }
+
+        private void HandleNumericKeyPress(Key key)
+        {
+            char numChar = (char) (key - Key.N0 + '0');
+
+            if (inClearedMode)
+            {
+                if (numChar == '0')
+                    return;
+
+                currentlyDisplayedString = string.Empty;
+                inClearedMode = false;
+            }
+
+            currentlyDisplayedString += numChar;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            display.SetText (currentlyDisplayedString);
+        }
+
+        private readonly IDisplay display;
+        private bool inClearedMode;
+        private string currentlyDisplayedString;
+    }
+}
---
+++ Experimental/CalculatorTdd/CalculatorTdd/Key.cs
@@ -0,0 +1,26 @@
+﻿namespace CalculatorTdd
+{
+    public enum Key
+    {
+        N0,
+        N1,
+        N2,
+        N3,
+        N4,
+        N5,
+        N6,
+        N7,
+        N8,
+        N9,
+        Dot,
+        Add,
+        Sub,
+        Mul,
+        Div,
+        Equals,
+        C,
+        MRC,
+        MPlus,
+        MMinus
+    }
+}
