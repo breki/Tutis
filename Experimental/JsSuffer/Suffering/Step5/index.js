d857a8a78e67cfee9341d5a8c11af6a805bdd2b2
igor <igor@localhost>
2018-12-08 18:15:03 +0100

    Added step 5.

---
+++ Experimental/JsSuffer/Suffering/Step5/index.js
@@ -0,0 +1,6 @@
+﻿var Calculator = require("./lib.js");
+
+export function onButtonClick() {
+    document.getElementById("text-div").innerText = "Hello world "
+        + Calculator.calculator();
+}
---
+++ Experimental/JsSuffer/Suffering/Step5/lib.js
@@ -0,0 +1,3 @@
+﻿export function calculator() {
+    return 678;
+}
