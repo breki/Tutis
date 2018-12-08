86ef3d70f5b32bcbb30a8da121e8c11660f64adc
igor <226863+breki@users.noreply.github.com>
2018-12-08 19:30:30 +0100

    Added step 8

---
+++ Experimental/JsSuffer/Suffering/Step8/index.ts
@@ -0,0 +1,13 @@
+﻿import L = require("leaflet");
+import Mapbox = require("mapbox.js");
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+const mapboxAccessToken = "XXX";
+Mapbox.mapbox.accessToken = mapboxAccessToken;
+var mapboxLayer = Mapbox.mapbox.tileLayer("mapbox.outdoors");
+mapboxLayer.addTo(map);
---
+++ Experimental/JsSuffer/Suffering/Step8/webpack.config.js
@@ -0,0 +1,21 @@
+﻿const path = require("path");
+
+module.exports = {
+    entry: "./index.ts",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+            ]
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        library: "EntryPoint",
+        //libraryTarget: "var"
+    }
+};
