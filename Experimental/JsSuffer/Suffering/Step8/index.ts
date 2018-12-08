94b4e0d19720a21b8e4a10b1e87b0c065fa11a6d
igor <igor@localhost>
2018-12-08 19:36:27 +0100

    More work in step 8

--- Experimental/JsSuffer/Suffering/Step8/index.ts
+++ Experimental/JsSuffer/Suffering/Step8/index.ts
@@ -1,4 +1,5 @@
 ﻿import L = require("leaflet");
+// @ts-ignore
 import Mapbox = require("mapbox.js");
 
 var map = L.map('map').setView([51.505, -0.09], 13);
@@ -7,7 +8,7 @@ L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 
-const mapboxAccessToken = "XXX";
+const mapboxAccessToken = "pk.eyJ1IjoiYnJla2kiLCJhIjoiNzM5Y0xKUSJ9.l2_sG-EyNgU-nWc1MiS6vw";
 Mapbox.mapbox.accessToken = mapboxAccessToken;
 var mapboxLayer = Mapbox.mapbox.tileLayer("mapbox.outdoors");
 mapboxLayer.addTo(map);
