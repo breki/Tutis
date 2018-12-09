5c0ee0cec54af096353e9f97accbd129044cc5a5
igor <226863+breki@users.noreply.github.com>
2018-12-09 16:06:51 +0100

    Fixing the step 8

--- Experimental/JsSuffer/Suffering/Step4JsNpm/webpack.config.js
+++ Experimental/JsSuffer/Suffering/Step4JsNpm/webpack.config.js
@@ -3,6 +3,7 @@
 module.exports = {
     entry: "./index.js",
     mode: "development",
+    devtool: "inline-source-map",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
--- Experimental/JsSuffer/Suffering/Step8/webpack.cmd
+++ Experimental/JsSuffer/Suffering/Step8/webpack.cmd
@@ -1,1 +1,1 @@
-node_modules\.bin\webpack --debug --display-reasons --display-origins --verbose --display-modules
+node_modules\.bin\webpack --debug --display-reasons --display-origins --verbose --display-modules --display-used-exports --display-provided-exports
