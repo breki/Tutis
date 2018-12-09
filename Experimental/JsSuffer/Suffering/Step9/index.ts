f5471ad0fd1cc830cb2c328cb1137ab199a527fd
igor <226863+breki@users.noreply.github.com>
2018-12-09 16:39:01 +0100

    Added step 9

---
+++ Experimental/JsSuffer/Suffering/Step9/index.ts
@@ -0,0 +1,46 @@
+﻿import Leaflet = require("leaflet");
+// @ts-ignore
+import Mapbox = require("mapbox.js");
+// @ts-ignore
+import Geocod = require("leaflet-control-geocoder");
+
+// declare module 'some-module-without-typings/*'
+
+var map = Leaflet.map('map').setView([51.505, -0.09], 13);
+
+Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+Leaflet.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+const mapboxAccessToken = "pk.eyJ1IjoiYnJla2kiLCJhIjoiNzM5Y0xKUSJ9.l2_sG-EyNgU-nWc1MiS6vw";
+Mapbox.mapbox.accessToken = mapboxAccessToken;
+var mapboxLayer = Mapbox.mapbox.tileLayer("mapbox.outdoors");
+mapboxLayer.addTo(map);
+
+const geocoder = new Geocod.Geocoder.Nominatim({});
+
+const geocoderLayer = Geocod.Control.geocoder({
+    collapsed: false,
+    geocoder,
+    placeholder: "Search for places...",
+});
+    //.on("markgeocode",
+    //    (e: any) => {
+    //        //const bbox = e.geocode.bbox;
+    //        //const poly = L.polygon([
+    //        //    bbox.getSouthEast(),
+    //        //    bbox.getNorthEast(),
+    //        //    bbox.getNorthWest(),
+    //        //    bbox.getSouthWest(),
+    //        //]);
+    //        //this.geocoderLayer.markGeocode(e);
+    //        //this.map.fitBounds(poly.getBounds());
+    //        //this.removeBox();
+    //    });
+
+geocoderLayer.addTo(map);
+
---
+++ Experimental/JsSuffer/Suffering/Step9/webpack.config.js
@@ -0,0 +1,36 @@
+﻿var path = require("path");
+var DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: "./index.ts",
+    mode: "development",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+            ]
+    },
+    resolve: {
+        alias: {
+            leaflet: path.resolve(__dirname, 'node_modules/leaflet')
+        }
+    },
+    plugins: [
+        new DuplicatePackageCheckerPlugin(),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false
+        })
+    ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        library: "EntryPoint",
+        //libraryTarget: "var"
+    }
+};
