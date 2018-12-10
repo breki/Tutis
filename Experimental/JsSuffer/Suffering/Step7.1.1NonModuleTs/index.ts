e943caaa141511d18b059c7ffb0f1186f9024160
igor <226863+breki@users.noreply.github.com>
2018-12-10 06:00:36 +0100

    Added step 7.1.1: TypeScript dealing with non-module js files and global dependencies like jQuery

---
+++ Experimental/JsSuffer/Suffering/Step7.1.1NonModuleTs/index.ts
@@ -0,0 +1,11 @@
+﻿// @ts-ignore
+import mylib = require("exports-loader?MyLib,MyLib.calculator!./nonmodule-lib.js");
+
+const ProperMyLib: any = mylib;
+
+export function onButtonClick() {
+    const labelEl = document.getElementById("text-div");
+    if (labelEl)
+        labelEl.innerText = "Hello world "
+            + ProperMyLib.MyLib.calculator();
+}
---
+++ Experimental/JsSuffer/Suffering/Step7.1.1NonModuleTs/webpack.config.js
@@ -0,0 +1,35 @@
+﻿const path = require("path");
+var DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+var BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: "./index.ts",
+    mode: "development",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: require.resolve("./nonmodule-lib.js"),
+                use: "imports-loader?$=jquery"
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new DuplicatePackageCheckerPlugin(),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false
+        })
+    ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        library: "EntryPoint"
+        //libraryTarget: "var"
+    }
+};
