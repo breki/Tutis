44d411e4587fa1042d6943e12c88e418baabdb29
igor <igor@localhost>
2018-12-10 05:46:37 +0100

    Added step 7.1: dealing with non-module js files and global dependencies like jQuery

---
+++ Experimental/JsSuffer/Suffering/Step7.1NonModule/index.js
@@ -0,0 +1,7 @@
+﻿//require("./nonmodule-lib.js");
+var mylib = require("exports-loader?MyLib,MyLib.calculator!./nonmodule-lib.js");
+
+export function onButtonClick() {
+    document.getElementById("text-div").innerText = "Hello world "
+        + mylib.MyLib.calculator();
+}
---
+++ Experimental/JsSuffer/Suffering/Step7.1NonModule/webpack.config.js
@@ -0,0 +1,30 @@
+﻿const path = require("path");
+var DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+var BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: "./index.js",
+    mode: "development",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: require.resolve("./nonmodule-lib.js"),
+                use: "imports-loader?$=jquery"
+            }
+        ]
+    },
+    plugins: [
+        new DuplicatePackageCheckerPlugin(),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            openAnalyzer: false
+        })
+    ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        library: "EntryPoint",
+        //libraryTarget: "var"
+    }
+};
